39e0dd71312f565a3ab982ad086e1ba6587117a4
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2024-03-23 21:55:17 +0900

    hsm-posix: update mount-gfarm2fs-and-lhsmtool_posix.sh

--- hsm-posix/mount-gfarm2fs-and-lhsmtool_posix.sh
+++ hsm-posix/mount-gfarm2fs-and-lhsmtool_posix.sh
@@ -17,18 +17,37 @@ DAEMON=
 # user_allow_other is required
 grep "^user_allow_other" /etc/fuse.conf
 
+ignore() {
+    return 0
+}
+
 clean() {
     GFS_MOUNTDIR=$GFS_MOUNTDIR GFARMFS_ROOT=$GFARMFS_ROOT \
-                umount.gfarm2fs || true
+                umount.gfarm2fs || ignore
+}
+
+INT=0
+
+interrupt() {
+    INT=1
 }
 
 clean
+trap interrupt INT
 trap clean EXIT
 
 gfmkdir -p $GFARMFS_ROOT
+
 mkdir -p $GFS_MOUNTDIR
+chmod 700 $GFS_MOUNTDIR
 chown $USER $GFS_MOUNTDIR
 
 GFS_MOUNTDIR=$GFS_MOUNTDIR GFARMFS_ROOT=$GFARMFS_ROOT \
             FUSE_OPTIONS="-o allow_root" mount.gfarm2fs
+set +e
 sudo $HSMTOOL $DAEMON --hsm-root $HSM_ROOT --archive=${ARCHIVE} $MNT_LUSTRE
+rv=$?
+if [ $INT -eq 1 ]; then
+    rv=0
+fi
+exit $rv
